cfddd0820ca568194c4edf921a99483509762574
Luke Runnels <lar9482@utulsa.edu>
2023-04-03 15:53:54 -0500

    Implemented impurity function for information gain

--- DecisionTree/DT.py
+++ DecisionTree/DT.py
@@ -4,6 +4,7 @@ from enum import Enum
 from DecisionTree.DT_node import DT_node
 
 import numpy as np
+import math
 
 class Info_Gain(Enum):
     Mis_classify = 0
@@ -42,6 +43,41 @@ class DT(Model):
         for attribute in attributes:
             pass
         pass
+
+    def impurity(self, examples):
+        all_classes = np.unique(examples[1])
+        all_point_class_fracts = []
+
+        #Initially calculate all fractions of points that belong to each class
+        for class_name in all_classes:
+            points_belonging_to_class = len(np.where(examples[1] == class_name)[0])
+            all_points = len(examples[1])
+
+            point_class_fract = (points_belonging_to_class / all_points)
+            all_point_class_fracts.append(point_class_fract)
+        
+        #SOURCE: Slide 16 from 'SupervisedLearningDecisionTree.pptx'
+        if (self.info_gain == Info_Gain.Mis_classify):
+            max_pointclass_fracs = max(all_point_class_fracts)
+            return 1 - max_pointclass_fracs
+        
+        elif (self.info_gain == Info_Gain.Entropy):
+            sum = 0
+            for point_class_fract in all_point_class_fracts:
+                sum += (point_class_fract) * np.log2(point_class_fract)
+
+            return -sum
+        
+        elif (self.info_gain == Info_Gain.Gini):
+            sum = 0
+            for point_class_fract in all_point_class_fracts:
+                sum += (point_class_fract) * (1 - point_class_fract)
+
+            return sum
+
+        else:
+            raise Exception('Impurity: Invalid information gain request made!')
+
 
     def plurality_value(self, Y):
         pass
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from utils.file_io import load_EMG_data, load_optdigits_data, load_spambase_data
 from utils.shuffle import shuffle
-from DecisionTree.DT import DT
+from DecisionTree.DT import DT, Info_Gain
 import numpy as np
 def main():
-    (X, Y) = load_spambase_data(4500)
+    (X, Y) = load_spambase_data(5000)
     (X, Y) = shuffle(X, Y)
 
-    tree = DT()
-    tree.fit(X, Y)
+    tree = DT(Info_Gain.Gini)
+    # tree.fit(X, Y)
+    test = tree.impurity((X, Y))
+    print(test)
 
 
 if __name__ == "__main__":
